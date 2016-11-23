3445c940ed8100f61e634328eda1f42bc8e2918a
Joel Busch <joel.busch@hotmail.com>
2016-11-23 15:49:52 +0100

    Added serialisation for Messages, not quite done

--- app/src/main/java/ch/ethz/inf/vs/a4/pascalo/vs_pascalo_chatapp/Message.java
+++ app/src/main/java/ch/ethz/inf/vs/a4/pascalo/vs_pascalo_chatapp/Message.java
@@ -1,21 +1,25 @@
 package ch.ethz.inf.vs.a4.pascalo.vs_pascalo_chatapp;
 
-import java.sql.Timestamp;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Message {
 
     private boolean writtenByMe;
 
-    private String msg;
+    private String message;
 
-    private Timestamp timestamp;
+    private Calendar timeSent;
 
     private boolean acked = false;
 
-    public Message(boolean w, String m, Timestamp t) {
+    public Message(boolean w, String m, Calendar s) {
         writtenByMe = w;
-        msg = m;
-        timestamp = t;
+        message = m;
+        timeSent = s;
     }
 
     public void setAcked() {
@@ -24,20 +28,48 @@ public class Message {
 
 
     // getter functions
-    public boolean getWrittenByus() {
+    public boolean getWrittenByMe() {
         return writtenByMe;
     }
 
-    public String getMsg() {
-        return msg;
+    public String getMessage() {
+        return message;
     }
 
-    public Timestamp getTimestamp() {
-        return timestamp;
+    public Calendar getTimeSent() {
+        return timeSent;
     }
 
 
     public boolean isAcked() {
         return acked;
+    }
+
+    public JSONObject getJsonRepresentationForStorage() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("writtenByMe", writtenByMe);
+            json.put("acked", acked);
+            json.put("message", message);
+            json.put("timeSent", timeSent.getTimeInMillis());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+    // Create a Message from the JSON representation, if it comes from the storage we have all fields
+    public Message(String string, boolean fromStorage) {
+        try {
+            JSONObject json = new JSONObject(string);
+            writtenByMe = json.getBoolean("writtenByMe");
+            acked = json.getBoolean("acked");
+            message = json.getString("message");
+            timeSent = new GregorianCalendar();
+            timeSent.setTimeInMillis(json.getLong("timeSent"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
