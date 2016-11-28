67ad9c736ec1d552b72aaea156e875757c75877c
Joel Busch <joel.busch@hotmail.com>
2016-11-28 19:13:47 +0100

    Started integrating the barcode scanner, scanning now seems to work, still need to define what format goes into the QR code and generate it, and eventually parse the results of a scan

--- app/src/main/java/ch/ethz/inf/vs/a4/pascalo/vs_pascalo_chatapp/UI/ScanKeyActivity.java
+++ app/src/main/java/ch/ethz/inf/vs/a4/pascalo/vs_pascalo_chatapp/UI/ScanKeyActivity.java
@@ -1,15 +1,46 @@
 package ch.ethz.inf.vs.a4.pascalo.vs_pascalo_chatapp.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import ch.ethz.inf.vs.a4.pascalo.vs_pascalo_chatapp.R;
+import ch.ethz.inf.vs.a4.pascalo.vs_pascalo_chatapp.ZXing.IntentIntegrator;
+import ch.ethz.inf.vs.a4.pascalo.vs_pascalo_chatapp.ZXing.IntentResult;
 
-public class ScanKeyActivity extends AppCompatActivity {
+public class ScanKeyActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_key);
+
+        Button scan = (Button) findViewById(R.id.button_scan_chat_partner_info);
+        scan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(ScanKeyActivity.this);
+                integrator.initiateScan();
+            }
+        });
+
+        Button add = (Button) findViewById(R.id.button_add_chat_partner);
+        add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            // handle scan result
+            EditText name = (EditText) findViewById(R.id.editText_partner_name);
+            name.setText(scanResult.getContents());
+        }
+        // else continue with any other code you need in the method
+
     }
 }
